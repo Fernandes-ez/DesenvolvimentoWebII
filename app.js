f997184892e27ffc332f1ae8c23dd03b9b72c85b
fernandes <enzo.dantas@hotmail.com>
2024-09-13 10:59:39 -0300

    Aula 6

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ app.use(bodyParser.json())
 
 app.post("/cadastrar", function(req, res){
     post.create({
-        nome: req.body.nome
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
     }).then(function(){
         console.log("Cadastrado com sucesso")
         res.send("Cadastrado com sucesso")
@@ -20,6 +24,14 @@ app.post("/cadastrar", function(req, res){
 })
 app.get("/", function(req, res){
     res.render("primeira_pagina")
+})
+app.get("/consulta", function(req, res){
+    post.findAll().then(function(posts){
+        res.render("consultar.handlebars", {posts: posts})
+    })
+})
+app.get("editar", function(req, res){
+    
 })
 app.get("/segunda", function(req, res){
     res.render("segunda_pagina")
